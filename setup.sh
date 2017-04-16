e504ceefecbbc094e0af27a6216c4ade0137985f
Aravind Anbudurai <aru777@gmail.com>
2017-04-15 23:02:19 -0700

    i3 configs

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CONFIG_DIR=$HOME/.config
+echo "Creating config dir if not available: $CONFIG_DIR"
+mkdir -p $CONFIG_DIR
+
+DOTFILES_DIR=$(pwd -P)
+
+# i3 config symlink
+ln -sf $DOTFILES_DIR/i3 $CONFIG_DIR
+
+# fonts
+mkdir -p $HOME/.fonts
+
+# Yosemite San Francisco Font
+cp $DOTFILES_DIR/YosemiteSanFranciscoFont/*.ttf $HOME/.fonts/
+
+#####
+##### Manual steps
+# Change gtk 2.0 and 3.0 settings manually for fonts
+# Get rofi
+
